228837bfd5ae237a7874b0c3d36c74938c15df14
Fernando-S <nandoo44@hotmail.com>
2017-10-29 11:24:18 -0200

    mudanca da fonte e tamanho da msg de game over

--- Snake/core/src/Source/GameView/GameOverHud.scala
+++ Snake/core/src/Source/GameView/GameOverHud.scala
@@ -1,13 +1,16 @@
 package Source.GameView
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.{Label, Table}
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 
 class GameOverHud {
+  var font: BitmapFont =_
+  font = new BitmapFont(Gdx.files.internal("fonts/Fonte.fnt"), false)
   var stage = new Stage
-  var gameOverLabel = new Label("Game Over", new Label.LabelStyle(new BitmapFont(), Color.RED))
+  var gameOverLabel = new Label("GAME OVER", new Label.LabelStyle(font, Color.RED))
   var table = new Table()
 
   table.top()
